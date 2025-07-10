9975db30ad8a3992c0788fcd87eee6d872ecef03
naif19ta <juan.patino.dc@gmail.com>
2025-07-09 23:13:32 -0500

    reading file

---
+++ source/Fatal.java
@@ -0,0 +1,20 @@
+public class Fatal
+{
+	public static void fileDoesNotExist (final String filename)
+	{
+		/* TODO */
+	}
+
+	public static void cannotReadFile (final String filename)
+	{
+		/* TODO */
+	}
+
+	public static void InputOutputInt (final String filename)
+	{
+	}
+
+	public static void uncompletedRead (final String filename, final long read, final long outof)
+	{
+	}
+}
--- source/Main.java
+++ source/Main.java
@@ -1,14 +1,52 @@
-
 import jxa.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class Main
 {
+	private static long sourceLength;
+	private static char source[];
+
 	final private static JxaFlag flags[] =
 	{
 		new JxaFlag("compile", 'c', JxaFlag.FlagArg.YES,           "compiling source"),
 		new JxaFlag("output",  'o', JxaFlag.FlagArg.MAY, "a.out",  "place the output in the file provided (a.out by default)"),
 		new JxaFlag("arch",    'a', JxaFlag.FlagArg.MAY, "x86_64", "arch to be used (x64 or ARM) (x64 by default)")
 	};
+
+	private static void handleFile (final String filename)
+	{
+		File file = new File(filename);
+
+		if (!file.exists())  { Fatal.fileDoesNotExist(filename); }
+		if (!file.canRead()) { Fatal.cannotReadFile(filename); }
+
+		try
+		{
+			FileReader reader = new FileReader(file);
+			sourceLength = file.length();
+			source = new char[(int) sourceLength];
+
+			final int bytesRead = reader.read(source, 0, (int) sourceLength);
+
+			if (bytesRead != sourceLength)
+			{
+				Fatal.uncompletedRead(filename, bytesRead, sourceLength);
+			}
+			reader.close();
+		}
+		catch (IOException why)
+		{
+			if (why instanceof FileNotFoundException)
+			{
+				Fatal.fileDoesNotExist(filename);
+			}
+			Fatal.InputOutputInt(filename);
+		}
+	}
+
 
 	public static void main (String args[])
 	{
@@ -21,5 +59,8 @@ public class Main
 			System.exit(0);
 		}
 
+		handleFile(compile);
+
+		System.out.println(source);
 	}
 }
