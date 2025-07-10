de4929bf0489b33161317fd16eff452b38209131
naif19ta <juan.patino.dc@gmail.com>
2025-07-09 23:22:19 -0500

    error handler

--- source/Fatal.java
+++ source/Fatal.java
@@ -2,19 +2,47 @@ public class Fatal
 {
 	public static void fileDoesNotExist (final String filename)
 	{
-		/* TODO */
+		final String fmt = String.format(
+			"bc: \u001b[31mfatal\u001b[0m: %s file does not exist\n" +
+			"compilation terminated.",
+			filename
+		);
+		System.err.println(fmt);
+		System.exit(1);
 	}
 
 	public static void cannotReadFile (final String filename)
 	{
-		/* TODO */
+		final String fmt = String.format(
+			"bc: \u001b[31mfatal\u001b[0m: %s file cannot be read\n" +
+			"compilation terminated.",
+			filename
+		);
+		System.err.println(fmt);
+		System.exit(1);
 	}
 
 	public static void InputOutputInt (final String filename)
 	{
+		final String fmt = String.format(
+			"bc: \u001b[31mfatal\u001b[0m: input/output interrupted error\n" +
+			"while working with %s file: aborting",
+			filename
+		);
+		System.err.println(fmt);
+		System.exit(1);
 	}
 
 	public static void uncompletedRead (final String filename, final long read, final long outof)
 	{
+		final String fmt = String.format(
+			"bc: \u001b[31mfatal\u001b[0m: cannot read 100% of %s file\n" +
+			"only %d out of %d bytes were read; aborting",
+			filename,
+			read,
+			outof
+		);
+		System.err.println(fmt);
+		System.exit(1);
 	}
 }
--- source/Main.java
+++ source/Main.java
@@ -60,7 +60,5 @@ public class Main
 		}
 
 		handleFile(compile);
-
-		System.out.println(source);
 	}
 }
